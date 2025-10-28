861e6070455d5a3e837c522477508c9dc1b6f47b
DadeHubs <DadeHubs@gmail.com>
2025-10-28 23:32:55 +0330

    Update config.js

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -6,17 +6,10 @@ export default defineConfig({
   description: "بهترین عسل‌های طبیعی و ارگانیک ایران",
   lang: 'fa-IR',
   themeConfig: {
-    // مسیردهی به استایل سفارشی
-    // این خط را اضافه کنید
-    custom: {
-      css: '/theme/style.css'
-    },
-    
-    // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'خانه', link: '/' },
-      { text: 'محصولات', link: '/products' }, // صفحه محصولات جدید
-      { text: 'تماس با ما', link: '/contact' } // صفحه تماس
+      { text: 'محصولات', link: '/products' },
+      { text: 'تماس با ما', link: '/contact' }
     ],
 
     sidebar: [
